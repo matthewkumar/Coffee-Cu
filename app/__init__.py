09f6c55b2d45e942f7781c1a92abe962fc8afcf5
jimmyro <jimmy@adicu.com>
2017-03-24 16:08:21 -0400

    removed extraneous comment

--- app/__init__.py
+++ app/__init__.py
@@ -9,15 +9,6 @@ app.secret_key = os.urandom(24)
 
 with open('config/databaseConfig.json', 'r') as databaseConfig:
     config = json.load(databaseConfig)
-    
-'''{
-  "apiKey": "AIzaSyB07PemCqeZDjBjgJu7dAOp6gUJI7KkirE",
-  "authDomain": "coffeecu-6f84a.firebaseapp.com",
-  "databaseURL": "https://coffeecu-6f84a.firebaseio.com",
-  "storageBucket": "coffeecu-6f84a.appspot.com",
-  "messagingSenderId": "896491819806",
-  "serviceAccount": "config/serviceAccount.json"
-}'''
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
