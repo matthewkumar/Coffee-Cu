d33fc77c842dd6635469da36875dbba2caaac05d
jimmyro <jimmy@adicu.com>
2017-03-24 16:07:48 -0400

    moved more info to config

--- app/__init__.py
+++ app/__init__.py
@@ -7,20 +7,23 @@ app = Flask(__name__)
 app.config.from_object('config')
 app.secret_key = os.urandom(24)
 
-config = {
+with open('config/databaseConfig.json', 'r') as databaseConfig:
+    config = json.load(databaseConfig)
+    
+'''{
   "apiKey": "AIzaSyB07PemCqeZDjBjgJu7dAOp6gUJI7KkirE",
   "authDomain": "coffeecu-6f84a.firebaseapp.com",
   "databaseURL": "https://coffeecu-6f84a.firebaseio.com",
   "storageBucket": "coffeecu-6f84a.appspot.com",
   "messagingSenderId": "896491819806",
-  "serviceAccount": "serviceAccount.json"
-}
+  "serviceAccount": "config/serviceAccount.json"
+}'''
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 db = firebase.database()
 
-with open('majors.json', 'r') as datafile:
-    majors = json.load(datafile)
+with open('majors.json', 'r') as majorsFile:
+    majors = json.load(majorsFile)
 
 from app import views
